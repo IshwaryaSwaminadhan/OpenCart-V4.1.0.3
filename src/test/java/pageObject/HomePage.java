c278311152ad91ce7ff2b314a4066ff12ff006be
Iswarya <iswaryas0310@gmail.com>
2026-03-12 17:08:40 +0530

    This is my second commit on this project

--- src/test/java/pageObject/HomePage.java
+++ src/test/java/pageObject/HomePage.java
@@ -13,7 +13,7 @@ public class HomePage extends BasePage{
 	}
 	
 	@FindBy(xpath="//span[normalize-space()='My Account']")
-    WebElement lnkonmyacc;
+    WebElement  lnkonmyacc;
 	
 	@FindBy(xpath="//a[normalize-space()='Register']")
 	WebElement clkregister;
